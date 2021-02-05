9ac0eb1e5550464c341f2257679bc5749f8652b5
ProgPedrao <pedrohrmartins98@gmail.com>
2021-02-05 16:26:03 -0300

    Removendo codigo de descobrir versão do AutoCAD
    Retirado o codigo inutilizado do Program.cs.

--- Impressao/Program.cs
+++ Impressao/Program.cs
@@ -17,57 +17,6 @@ namespace Impressao
         [STAThread]
         public static void Main(string[] args)
         {
-            RegistryKey autocad = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Autodesk\AutoCAD");
-            string autocadCurver = null;
-            string[] VersoesAutocad = { "R22.0" , "R23.0", "R23.1" };
-            object autocadLang = autocad.GetValue("CurVer");
-            int versao = 0; 
-
-            for (int i = 0; i < VersoesAutocad.Length; i++)
-            {
-                autocad = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Autodesk\AutoCAD\" + VersoesAutocad[i] + @"\", false);
-                if (autocad != null)
-                {
-                    autocadCurver = VersoesAutocad[i];
-                    autocad = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Autodesk\AutoCAD\" + autocadCurver + @"\");
-                    autocadLang = autocad.GetValue("CurVer");
-
-                    switch (autocadLang)
-                    {
-                        case "ACAD-1001:409":
-                            versao = 2018;
-                            autocadCurver = "R22.0";
-                            break;
-                        case "ACAD-2001:409":
-                            versao = 2019;
-                            autocadCurver = "R23.0";
-                            break;
-                        case "ACAD-3026:409":
-                            versao = 2020;
-                            autocadCurver = "R23.1";
-                            break;
-                        default:
-                            MessageBox.Show("Versão incompatível com o software");
-                            Application.Exit();
-                            break;
-                    }
-                    autocad = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Autodesk\AutoCAD\" + autocadCurver + @"\");
-                    if (autocadLang.Equals(autocad.GetValue("CurVer")))
-                    {
-                        break;
-                    }
-                    else { }
-                }
-                else
-                {
-                }
-            }
-
-
-
-
-            MessageBox.Show(autocadLang + "; versão: " + versao);
-
             List<string> arq = new List<string>();
             bool format = true;
 
